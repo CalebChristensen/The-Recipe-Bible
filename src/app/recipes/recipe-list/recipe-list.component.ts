672fdcedddb0dc46c88ad696f82e0ae18de5ec21
Caleb Christensen <Caleb.Christ@gmail.com>
2019-02-14 16:30:06 -0500

    recipe list on the right

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://hips.hearstapps.com/vidthumb/images/delish-marry-me-chicken-1523047305.png')
+    new Recipe('A Test Recipe 1', 'This is simply a test', 'https://hips.hearstapps.com/vidthumb/images/delish-marry-me-chicken-1523047305.png'),
+    new Recipe('A Test Recipe 2', 'This is simply a test', 'https://www.incredibleegg.org/wp-content/uploads/RecipeDetail_Deviled-930x550.jpg'),
   ];
   constructor() { }
 
